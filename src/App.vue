cf830ffa42b0c42d05ddf716b96a3e7b225c1cc3
LuciaXu <3012170145@qq.com>
2022-10-28 21:45:55 +0800

    引入组件库

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
     132
+    <i class="toutiao toutiao-shouye"></i>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-
+export default {
+  name: 'App'
+}
 </script>
 
-<style lang="less">
+<style scoped lang="less">
 
 </style>
